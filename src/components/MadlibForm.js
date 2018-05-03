60e7c44860e3a2d2ca1a491eef9abc9d9db4473a
Greg Altman <greg@flocabulary.com>
2018-05-03 19:01:28 -0400

    functionality works ok

--- src/components/MadlibForm.js
+++ src/components/MadlibForm.js
@@ -1,38 +1,105 @@
 import React, { Component } from 'react';
+const regex = /%&(.*?)&%/gi;
 
 class MadlibForm extends Component {
   state = {
-    fields: (
-      regex => {
-        const fields = {};
-        let result;
-        while (result = regex.exec(this.props.madlib)) {
-          fields[result[1]] = '';
-        }
-        return fields;
-      }
-    )(/%&(.*?)&%/gi)
-  }
+    fields: this.getFormFieldsFromMadlib(),
+    submitted: false
+  };
 
-  render() {
+  getFormFieldsFromMadlib() {
+    const fields = {};
+    let result = regex.exec(this.props.madlib);
+    while (result) {
+      fields[result[1]] = '';
+      result = regex.exec(this.props.madlib);
+    }
+    return fields;
+  };
+
+  initializeMadlibForm = () => {
+    this.setState({
+      fields: this.getFormFieldsFromMadlib(),
+      submitted: false
+    })
+  };
+
+  submitDisabled() {
+    const {fields} = this.state;
+    return Object.keys(fields).filter(key => !fields[key]).length
+  };
+
+  renderMadlibForm() {
+    const {fields} = this.state;
     return (
       <div className='madlib-form'>
         <h2>Fill out the form below to create your madlib</h2>
-        <form onSubmit={this.onSubmit}>
+        <form onSubmit={e => this.setState({submitted: true})}>
           {
-            Object.keys(this.state.fields).map(
+            Object.keys(fields).map(
               field => (
                 <label>
                   {field}: <input
-                    value={this.state.fields[field]}
-                    onChange={e => this.setState({fields: Object.assign(this.state.fields, {[field]: e.target.value})})}
+                    value={fields[field]}
+                    onChange={
+                      e => this.setState({
+                        fields: Object.assign(fields, {[field]: e.target.value})
+                      })
+                    }
                   />
                 </label>
               )
-            ) 
+            )
           }
+          <button type='submit' disabled={this.submitDisabled()}>submit</button>
         </form>
       </div>
+    );
+  };
+
+  renderFilledInMadlib() {
+    const {fields} = this.state;
+    const {madlib} = this.props;
+    return (
+      <div className='madlib-filled-in'>
+        {
+          madlib.split('\n').map(
+            (line, i) => (
+              <span
+                key={`madlibline${i}`}
+                className='madlib-line'
+              >
+                {
+                  line.split(regex).map(
+                    chunk => (
+                      <span
+                        key={`${chunk}${i}`}
+                        className={
+                          fields[chunk]
+                          ? 'user-submitted-value'
+                          : ''
+                        }
+                      >
+                        {fields[chunk] || chunk}
+                      </span>
+                    )
+                  )
+                }
+              </span>
+            )
+          )
+        }
+        <button onClick={this.initializeMadlibForm}>submit</button>
+      </div>
+    )
+  };
+
+  render() {
+    const {submitted} = this.state;
+    return (
+      submitted
+      ? this.renderFilledInMadlib()
+      : this.renderMadlibForm()
     );
   }
 }
