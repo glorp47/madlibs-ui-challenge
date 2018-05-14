3b004e0338b75e7f8e6d5fe0d10e3cacf02dd646
Greg Altman <greg@flocabulary.com>
2018-05-14 19:19:18 -0400

    building out more error stuff

--- src/components/MadlibForm.js
+++ src/components/MadlibForm.js
@@ -2,40 +2,75 @@ import React, { Component } from 'react';
 const regex = /%&(.*?)&%/gi;
 
 class MadlibForm extends Component {
-  state = {
-    fields: this.getFormFieldsFromMadlib(),
-    submitted: false
+  state = this.getInitialState();
+
+  getInitialState() {
+    return {
+      // initialize error messages object with default value so
+      // form cannot be submitted before user input
+      fields: this.getFormFieldsFromMadlib(),
+      errorMessages: {},
+      fieldsTouched: [],
+      submitted: false
+    };
   };
 
   getFormFieldsFromMadlib() {
     const fields = {};
     let result = regex.exec(this.props.madlib);
     while (result) {
+      // null represents value before user input
       fields[result[1]] = '';
       result = regex.exec(this.props.madlib);
     }
     return fields;
   };
 
-  initializeMadlibForm = () => {
-    this.setState({
-      fields: this.getFormFieldsFromMadlib(),
-      submitted: false
-    })
+  initializeMadlibForm() {
+    this.setState(this.getInitialState());
   };
 
   submitDisabled() {
-    const {fields} = this.state;
-    return Object.keys(fields).filter(key => !this.validateField(key, fields[key])).length;
+    const {errorMessages, fields, fieldsTouched} = this.state;
+    return (
+      // submit disabled if not all fields have been touched
+      fieldsTouched.length !== Object.keys(fields).length ||
+      // or there are error messages for fields
+      Object.keys(errorMessages).filter(field => errorMessages[field].length).length
+    );
   };
 
   validateField(fieldName, value) {
+    const {fieldsTouched} = this.state;
+    // don't validate untouched fields
+    if (fieldsTouched.indexOf(fieldName) === -1) return;
+
+    const errors = [];
+
     // default validation: value cannot be blank.
-    return !!value;
+    if (!value) {
+      errors.push(
+        'Field cannot be blank'
+      );
+    }
+    return errors;
+  };
+
+  setErrorMessages() {
+    const {fields} = this.state;
+    this.setState({
+      errorMessages: Object.keys(fields).reduce(
+        (errors, fieldName) => Object.assign(
+          errors,
+          {[fieldName]: this.validateField(fieldName, fields[fieldName])}
+        ),
+        {}
+      )
+    });
   };
 
   renderMadlibForm() {
-    const {fields} = this.state;
+    const {errorMessages, fields, fieldsTouched} = this.state;
     return (
       <div className='madlib-form'>
         <h2>Fill out the form below to create your madlib</h2>
@@ -43,16 +78,30 @@ class MadlibForm extends Component {
           {
             Object.keys(fields).map(
               field => (
-                <label key={field}>
-                  {field}: <input
-                    value={fields[field]}
-                    onChange={
-                      e => this.setState({
-                        fields: Object.assign({}, fields, {[field]: e.target.value})
-                      })
-                    }
-                  />
-                </label>
+                <div key={field}>
+                  <label>
+                    {field}: <input
+                      value={fields[field]}
+                      onChange={
+                        e => this.setState({
+                          fields: Object.assign({}, fields, {[field]: e.target.value}),
+                          fieldsTouched: fieldsTouched.filter(
+                            touchedField => touchedField !== field
+                          ).concat([field])
+                        }, this.setErrorMessages)
+                      }
+                    />
+                  </label>
+                  {
+                    errorMessages[field] && errorMessages[field].map(
+                      error => error && (
+                        <span key={error} className='error'>
+                          {error}
+                        </span>
+                      )
+                    )
+                  }
+                </div>
               )
             )
           }
